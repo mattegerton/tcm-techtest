589d3af5a8d9a96ac203aec1b71c58aa925ce512
mattegerton <matthewjegerton@googlemail.com>
2018-11-25 14:28:24 +0000

    install dep lodash to fix comment replies

--- tech-test/src/components/Comments/ReplyComment.js
+++ tech-test/src/components/Comments/ReplyComment.js
@@ -1,29 +1,52 @@
 import React, { Component } from 'react';
 import '../../css/ReplyComment.css'
+const isEmpty = require('lodash.isempty');
 
 class ReplyComment extends Component {
     render() {
-        return (
-            <div className="container" id="replyCommentContainer">
-                <div> 
-                    <img id="replyAvatar" src={`https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`} alt="user avatar"/>
-                </div>
-                <div>
-                    <div id="replyUserName">
-                        <p id="replyUser"> Sarah Connor </p>
-                        <p id="replyTime"> 20 minutes ago </p>
+        return isEmpty(this.props.replies) ? null :
+            (
+                <div className="container" id="replyCommentContainer">
+                    <div>
+                        <img id="replyAvatar" src={this.props.replies.avatar} alt="user avatar" />
                     </div>
-                    <div id="replyBody"> 
-                        <p> Agreed! The players responded really well to it. </p>
-                    </div>
-                    <div id="replyInteractions" className="actionText"> 
-                        <div className="actionText">
-                            <i className="icon ion-md-thumbs-up"></i> <p> </p>
+                    <div>
+                        <div id="replyUserName">
+                            <p id="replyUser"> {this.props.replies.user} </p>
+                            <p id="replyTime"> 20 minutes ago </p>
+                        </div>
+                        <div id="replyBody">
+                            <p> {this.props.replies.body} </p>
+                        </div>
+                        <div id="replyInteractions" className="actionText">
+                            <div className="actionText">
+                                <i className="icon ion-md-thumbs-up"></i> <p> </p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        );
+            )
+        // return (
+        //     <div className="container" id="replyCommentContainer">
+        //         <div>
+        //             <img id="replyAvatar" src={this.props.replies.avatar} alt="user avatar" />
+        //         </div>
+        //         <div>
+        //             <div id="replyUserName">
+        //                 <p id="replyUser"> {this.props.replies.user} </p>
+        //                 <p id="replyTime"> 20 minutes ago </p>
+        //             </div>
+        //             <div id="replyBody">
+        //                 <p> {this.props.replies.body} </p>
+        //             </div>
+        //             <div id="replyInteractions" className="actionText">
+        //                 <div className="actionText">
+        //                     <i className="icon ion-md-thumbs-up"></i> <p> </p>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </div>
+        // )
     }
 }
 
--- tech-test/src/components/CommentsComponent.js
+++ tech-test/src/components/CommentsComponent.js
@@ -5,12 +5,36 @@ import CommentPost from './Comments/CommentPost';
 import NewComments from './Comments/NewComments';
 
 class CommentsComponent extends Component {
+
+    state = {
+        comments: [
+            {
+                user: 'David Smith',
+                avatar: `https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`,
+                body: 'Thought this worked out well last night, we should do it again next week',
+                replies:
+                {
+                    user: 'Sarah Connor',
+                    avatar: `https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`,
+                    body: 'Agreed! The players responded really well to it.'
+                }
+            },
+            {
+                user: 'Larry Moore',
+                avatar: 'https://images.pexels.com/photos/935969/pexels-photo-935969.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+                body: 'My lads loved this drill. Will definitely be doing it again, possible adding on or two more defenders to make it harder.',
+                replies: {
+
+                }
+            }
+        ]
+    }
     render() {
         return (
             <div id="commentsContainer">
                 <CommentHeader />
-                <CommentPost user={this.props.user}/>
-                <NewComments />
+                <CommentPost user={this.props.user} />
+                <NewComments comments={this.state.comments} />
             </div>
         );
     }
